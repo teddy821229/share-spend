2a65777c5042d697ed7d046b01d2bbe4d42b4fa9
teddy <a21506qoo@gmail.com>
2021-07-27 22:33:40 +0800

    feat: group-card

---
+++ src/components/GroupCard.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="card">
+    <router-link to="/">
+      <div class="img-container"></div>
+      <div class="name-container">Teddy地獄組</div>
+      <div class="group-detail">
+        <div class="member-container">
+          <ion-icon name="person-outline"></ion-icon>
+          <div class="member-count sub-title">5 位成員</div>
+        </div>
+        <div class="consume-container">
+          <ion-icon name="receipt-outline"></ion-icon>
+          <div class="consume-count sub-title">30 筆消費</div>
+        </div>
+        <div class="owed-container">
+          <ion-icon name="wallet-outline"></ion-icon>
+          <div class="owed sub-title">
+            + $3,667
+          </div>
+        </div>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ConsumeCard",
+};
+</script>
+
+<style scoped>
+.card {
+  height: 200px;
+  width: 90%;
+  max-width: 500px;
+
+  margin: 20px 0;
+
+  border: 2px solid #a9b6cc;
+  border-radius: 15px;
+}
+
+.card a {
+  width: 90%;
+  height: 90%;
+
+  margin: auto auto 5px auto;
+
+  display: grid;
+  grid-template-columns: 4fr 6fr;
+  grid-template-rows: 7fr 3fr;
+  align-items: center;
+
+  cursor: pointer;
+}
+
+.img-container {
+  grid-column: 1/2;
+  grid-row: 1/2;
+  justify-self: center;
+  width: 108px;
+  height: 108px;
+  border: 4px solid #a9b6cc;
+  border-radius: 50%;
+}
+
+.name-container {
+  grid-column: 2/3;
+  grid-row: 1/2;
+
+  color: #6784b4;
+  font-size: 30px;
+}
+
+.group-detail {
+  grid-column: 1/3;
+  grid-row: 2/3;
+  height: 30%;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  font-size: 18px;
+  color: #6784b4;
+}
+
+.member-container, .consume-container, .owed-container  {
+  display: flex;
+  align-items: center;
+}
+
+ion-icon {
+  font-size: 20px;
+}
+
+.consume-container {
+  justify-content: center;
+}
+
+.owed-container {
+  justify-content: flex-end
+}
+
+
+
+.sub-title {
+  padding-left: 10px;
+}
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,111 +1,159 @@
 <template>
   <div class="main-content">
-    <TopNavBar 
-      msg="首頁"
-    />
+    <TopNavBar msg="首頁" />
     <div class="row">
       <div class="col-5">
-        <div class="title">
-          最新消費
+        <div class="title">最新消費</div>
+        <div class="card-container">
+          <ConsumeCard
+            v-for="consume in consumes"
+            :key="consume.id"
+            :consume="consume"
+          />
         </div>
-        <ConsumeCard 
-          v-for="consume in consumes"
-          :key="consume.id"
-          :consume="consume"
-        />
       </div>
       <div class="col-7">
-        <div class="title">
-          分帳群組
+        <div class="title">分帳群組</div>
+        <div class="group-container">
+          <GroupCard />
         </div>
       </div>
     </div>
-    
   </div>
 </template>
 
 <script>
-import TopNavBar from './../components/TopNavBar.vue'
-import ConsumeCard from '../components/ConsumeCard.vue'
+import TopNavBar from "./../components/TopNavBar.vue";
+import ConsumeCard from "../components/ConsumeCard.vue";
+import GroupCard from './../components/GroupCard.vue'
 
 const dummyConsumes = [
   {
     id: 1,
-    category: 'food',
-    name: '午餐麥當勞歡樂送送送到你家哈哈哈哈',
+    category: "food",
+    name: "午餐麥當勞歡樂送送送到你家哈哈哈哈",
     amount: 500,
-    date: '2021/07/07'
+    date: "2021/07/07",
   },
   {
     id: 2,
-    category: 'entertainment',
-    name: '盲盒',
+    category: "entertainment",
+    name: "盲盒",
     amount: 700,
-    date: '2021/07/03'
+    date: "2021/07/03",
   },
   {
     id: 3,
-    category: 'life',
-    name: '洗髮水',
+    category: "life",
+    name: "洗髮水",
     amount: 120,
-    date: '2021/07/01'
+    date: "2021/07/01",
   },
-
-]
+  {
+    id: 4,
+    category: "food",
+    name: "午餐",
+    amount: 261,
+    date: "2021/07/02",
+  },
+  {
+    id: 5,
+    category: "life",
+    name: "洗面乳",
+    amount: 120,
+    date: "2021/06/28",
+  },
+  {
+    id: 6,
+    category: "entertainment",
+    name: "電影",
+    amount: 200,
+    date: "2021/06/23",
+  },
+  {
+    id: 7,
+    category: "life",
+    name: "高鐵",
+    amount: 1300,
+    date: "2021/06/01",
+  },
+  {
+    id: 8,
+    category: "entertainment",
+    name: "電影",
+    amount: 200,
+    date: "2021/06/23",
+  },
+  {
+    id: 9,
+    category: "life",
+    name: "高鐵",
+    amount: 1300,
+    date: "2021/06/01",
+  },
+];
 
 export default {
   name: "Home",
   components: {
     TopNavBar,
-    ConsumeCard
+    ConsumeCard,
+    GroupCard
   },
   created() {
-    this.fetchConsume()
+    this.fetchConsume();
   },
   data() {
     return {
-      consumes: []
-    }
+      consumes: [],
+    };
   },
   methods: {
     fetchConsume() {
-      this.consumes = dummyConsumes
-    }
-  }
-  
+      this.consumes = dummyConsumes;
+    },
+  },
 };
 </script>
 
 
 <style scoped>
-  .main-content {
-    height: 100%;
-    width: 100%;
-  }
+.main-content {
+  height: 100%;
+  width: 100%;
+}
 
-  .row {
-    height: calc(100% - 100px);
-  }
+.row {
+  height: calc(100% - 100px);
+}
+.col-5, .col-7 {
+  height: calc(100% - 54px);
+}
 
-  .title {
-    position: relative;
+.title {
+  position: relative;
 
-    padding: 10px 0 20px 0;
-    margin-bottom: 30px;
-    font-size: 16px;
-    color: #6784b4;
-    font-weight: 700;
-  }
+  padding: 10px 0 20px 0;
+  font-size: 16px;
+  color: #6784b4;
+  font-weight: 700;
+}
 
-  .title::after {
-    position: absolute;
-    left: 0;
-    bottom: 0;
+.title::after {
+  position: absolute;
+  left: 0;
+  bottom: 0;
 
-    content: '';
-    width: 85%;
-    height: 2px;
+  content: "";
+  width: 85%;
+  height: 2px;
 
-    background: #a9b6cc;
-  }
+  background: #a9b6cc;
+}
+
+.card-container, .group-container {
+  padding-top: 10px;
+  height: 100%;
+  overflow: scroll;
+}
 </style>
